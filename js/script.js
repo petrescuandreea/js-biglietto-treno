c6283b702e431446bd1dc85bfad0e46f15dfc50c
Andreea1696 <andreeapetrescu16@gmail.com>
2021-10-22 16:35:55 +0200

    msg

--- js/script.js
+++ js/script.js
@@ -23,29 +23,37 @@ console.log("Age of the passenger:", age);
 
 
 // Ticket cost
-let ticketCost = km * unitPrice;
-const underageDiscount = 0.2;
-const over65Discount = 0.4;
-let totalPrice;
+let price = (km * unitPrice);
 
 
 if (age < 18) {
 
-    totalPrice = (ticketCost - underageDiscount).toFixed(2);
-    element.innerHTML=`The ticket price is: ${totalPrice} &#8364;`;
+    price = (price - (price * 0.2));
+    element.innerHTML=
+    `
+    The base price of your train ticket is: &#8364;${price.toFixed(2)},
+    because you are underage you are entitled to a 20% discount.
+
+    You have to pay  &#8364;${price.toFixed(2)} for your train ticket.
+    `;
 
 } else if (age >= 65) {
 
-    totalPrice = (ticketCost - over65Discount).toFixed(2);
-    element.innerHTML=`The ticket price is: ${totalPrice} &#8364;`;
+    price = (price - (price * 0.4));
+    element.innerHTML=
+    `The base price of your train ticket is: &#8364;${price.toFixed(2)},
+    because you are over 65 you are entitled to a 40% discount.
+    
+    You have to pay  &#8364;${price.toFixed(2)} for your train ticket.
+    `;
 
 } else {
 
-    totalPrice = (ticketCost).toFixed(2);
-    element.innerHTML=`The ticket price is: ${totalPrice} &#8364;`;
+    price = (price);
+    element.innerHTML=`The ticket price is: &#8364;${price.toFixed(2)}`;
 
 }
 
 
-console.log(`The ticket price is: ${totalPrice} euro`);
+console.log(`The ticket price is: ${price} euro`);
 
